87d130432b5093f24b4ddd53a3cbe58337c5d88d
Ricardo Casanova <ricardo@pijp.eu>
2019-03-29 19:42:38 +0100

    Add some predefined methods to the view controller

--- VIPER Architecture.xctemplate/___FILEBASENAME___ViewController.swift
+++ VIPER Architecture.xctemplate/___FILEBASENAME___ViewController.swift
@@ -14,6 +14,43 @@ class ___VARIABLE_moduleName___ViewController: UIViewController {
     
 }
 
+// MARK: - Setup views
+extension ___VARIABLE_moduleName___ViewController {
+    
+    /**
+     * Setup views
+     */
+    private func setupViews() {
+        //__ Configure your view here
+        //__ Background color, title, safe area
+        
+        configureSubviews()
+        addSubviews()
+    }
+    
+    /**
+     * Configure subviews
+     */
+    private func configureSubviews() {
+        //__ Configure all the subviews here
+    }
+    
+}
+
+// MARK: - Layout & constraints
+extension ___VARIABLE_moduleName___ViewController {
+    
+    /**
+     * Add subviews
+     */
+    private func addSubviews() {
+        //__ Add all the subviews here
+        
+        //__ Configure the constraints
+    }
+
+}
+
 // MARK: - ___VARIABLE_moduleName___ViewInjection
 extension ___VARIABLE_moduleName___ViewController: ___VARIABLE_moduleName___ViewInjection {
     //__ Implement your protocols here
